4e794a822ef708d332c5a8070fe8742598726036
Credomane <github@credomane.com>
2018-02-02 15:13:11 -0600

    Add StdLib.cs to build process

--- ScriptingMod/ScriptEngines/StdLib/StdLib.cs
+++ ScriptingMod/ScriptEngines/StdLib/StdLib.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace ScriptingMod.ScriptEngines
 {
+    //TODO: Might need to create a JsStdLib and LuaStdLib for marshalling data to and from StdLib proper.
     class StdLib
     {
         public StdLib()
